171615d6a326a74ef4567411df7010157dc8691a
assilbekov <askhat.assilbekov@gmail.com>
2024-02-14 00:23:56 -0300

    fix

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,4 +1,4 @@
-import { useGetPostQuery } from "../store/slices";
+import { useGetPostQuery, useGetUserQuery } from "../store/slices";
 
 type PostPageProps = {
   postId: number;
@@ -6,6 +6,7 @@ type PostPageProps = {
 
 export const PostPage: React.FC<PostPageProps> = ({ postId }) => {
   const { data: post, error, isLoading } = useGetPostQuery(postId);
+  const {data: user} = useGetUserQuery(post?.userId || 0, {skip: !post});
 
   return (
     <div>
@@ -16,6 +17,12 @@ export const PostPage: React.FC<PostPageProps> = ({ postId }) => {
         <div>
           <h2>{post.title}</h2>
           <p>{post.body}</p>
+        </div>
+      )}
+      {user && (
+        <div>
+          <h2>Author</h2>
+          <p>{user.name}</p>
         </div>
       )}
     </div>
